35ab6c91ffdc583e9bad889d98307e2bdbf5b4ec
MadhanKumaryk <madhankumaryk@gmail.com>
2022-02-21 07:59:54 +0530

    commit

---
+++ Madhan Kumar/FrondEnd/Csharp/DLithe_BC_NFS_T_Task25_C#/Internal/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Internal_Access_Specifier
+{
+     public class access
+    {
+        // String Variable declared as internal
+       internal string name;
+        public void print()
+        {
+            Console.WriteLine("\nMy name is " + name);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            access ac = new access();
+            Console.Write("Enter your name:\t");
+            // Accepting value in internal variable
+            ac.name = Console.ReadLine();
+            ac.print();
+            
+        }
+    }
+}
